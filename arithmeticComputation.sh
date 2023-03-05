aa4d7dd3e97ca3cfc6e2be210a446874f4545353
Neha1659 <nehareddyanthireddy@gmail.com>
2023-03-05 09:17:17 +0530

    Add:Compute Values & Assume Part Time as 8 hrs

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,5 +1,15 @@
 read -p "Enter a value:" a
 read -p "Enter b value:" b
 read -p "Enter c value:" c
-result=$(($a + $b * $c))
-echo $result
+result1=$(($a + $b * $c))
+echo "result1="$result1
+result2=$(($a * $b + $c))
+echo "result2="$result2
+isPartTime=1
+randomCheck=$((RANDOM%2))
+if [ $isPartTime -eq $randomCheck ]
+then
+    echo empWrkHrs=8;
+else
+    echo empWrkHrs=0;
+fi
